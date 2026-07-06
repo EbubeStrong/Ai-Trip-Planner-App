f4450c309c2f281e374d4645ff3568b8cf5ca702
Abraham Samuel <ebubesammy567@gmail.com>
2026-07-06 22:44:33 +0100

    feat: implement UserDetailContext and integrate into Provider component

--- src/lib/provider.tsx
+++ src/lib/provider.tsx
@@ -1,9 +1,10 @@
 "use client"
 import Header from '@/components/Layouts/Header';
-import React, { useEffect, useCallback } from 'react'
+import React, { useEffect, useCallback, useState, useContext } from 'react'
 import { api } from '../../convex/_generated/api';
 import { useMutation } from 'convex/react';
 import { useUser } from '@clerk/nextjs';
+import { UserDetailContext } from '@/services/context/UserDetailContext';
 
 function Provider({
   children,
@@ -32,6 +33,7 @@ function Provider({
   const createUser = useMutation(api.user.createNewUser);
 
   const { user, isLoaded } = useUser();
+  const [userDetails, setUserDetails] = useState<unknown>(null);
 
   const createNewUser = useCallback(async () => {
     if (!user) return;
@@ -50,14 +52,23 @@ function Provider({
   }, [isLoaded, user, createNewUser]);
 
   return (
-    <div>
-      <Header />
-      <main className="mt-10">
-        {children}
-      </main>
-
-    </div>
+    <UserDetailContext.Provider value={{ userDetails, setUserDetails }}>
+      <>
+        <Header />
+        <main className="mt-10">
+          {children}
+        </main>
+      </>
+    </UserDetailContext.Provider>
   )
 }
-
 export default Provider
+
+
+export const useUserDetails = () => {
+  const context = useContext(UserDetailContext);
+  if (!context) {
+    throw new Error("useUserDetails must be used within a UserDetailContext.Provider");
+  }
+  return context;
+}
